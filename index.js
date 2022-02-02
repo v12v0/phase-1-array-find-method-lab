9bc974209466d2d012614086c8ac95525dae27db
Vi Vo <aaronvo118@gmail.com>
2022-02-02 13:55:32 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    let result = record.find(record => record.result === "W")
+    if (result) {
+        return result.year
+    } else {
+        return undefined
+    }
+}
